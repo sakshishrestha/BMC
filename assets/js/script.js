1573cd9df6004f681bdc68c58bf79cd6e7e43a6c
Sakshi Shrestha <sakshi@8squarei.com>
2022-07-05 21:48:49 +0545

    add house.html for new UI changes

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+let valueDisplays = document.querySelectorAll(".number");
+let interval = 5000;
+
+valueDisplays.forEach((valueDisplay) => {
+    let startValue = 0;
+    let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+    // console.log(endValue);
+    let duration = Math.floor(interval / endValue);
+    let counter = setInterval(function () {
+        startValue += 1;
+        valueDisplay.textContent = startValue;
+        if(startValue == endValue){
+            clearInterval(counter);
+        }
+    }, duration);
+});
+
+
